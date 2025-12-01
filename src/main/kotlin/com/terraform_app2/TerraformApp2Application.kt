a59a68460c6fdc81d53e4573b239ab14eeba2679
woongcheol Na <32201345@dankook.ac.kr>
2025-12-01 12:40:18 +0900

    init

---
+++ src/main/kotlin/com/terraform_app2/TerraformApp2Application.kt
@@ -0,0 +1,11 @@
+package com.terraform_app2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TerraformApp2Application
+
+fun main(args: Array<String>) {
+    runApplication<TerraformApp2Application>(*args)
+}
---
+++ src/test/kotlin/com/terraform_app2/TerraformApp2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.terraform_app2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TerraformApp2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
